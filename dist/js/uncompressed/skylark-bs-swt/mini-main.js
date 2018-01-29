719f5441c4d73cbf0c38d4672e12903d925fc234
lihongwang <lhw@tech.hudaokeji.com>
2018-01-29 14:05:34 +0800

    update datePicker with pastDates

---
+++ dist/js/uncompressed/skylark-bs-swt/mini-main.js
@@ -0,0 +1,45 @@
+/**
+ * skylark-bs-swt - The skylark bootstrap standard widget tookit
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0-beta
+ * @link https://github.com/skylarkui/skylark-bs-swt/
+ * @license MIT
+ */
+define([
+    // "skylark-utils/query",
+    // "./affix",
+    // "./alert",
+    // "./button",
+    "./carousel",
+    "./checkbox",
+    "./collapse",
+    "./combobox",
+    "./datepicker",
+    "./dropdown",
+    // "./dropdown-autoflip",
+    // "./infinite-scroll",
+    "./modal",
+    // "./picker",
+    // "./pillbox",
+    // "./placard",
+    // "./popover",
+    // "./radio",
+    "./repeater",
+    "./repeater-list",
+    "./repeater-thumbnail",
+    // "./scheduler",
+    // "./scrollspy",
+    "./search",
+    "./selectlist",
+    // "./spinbox",
+    "./tab",
+    // "./toolbar",
+    // "./tooltip",
+    "./transition",
+    // "./tree",
+    // "./window",
+    "./wizard",
+    // "./lightbox"
+], function($) {
+    return $;
+});
